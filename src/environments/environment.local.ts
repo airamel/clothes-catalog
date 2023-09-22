d4987221d2ff080b482a7117903830d3e6108e6d
Maria Melchor <m3l.dur@gmail.com>
2023-09-22 11:54:15 -0600

    feat: Added with secret variable in  enviroment.ts.

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  spreadsheetId: process.env.SPREADSHEETAPIKEY,
+  spreadsheetName: 'inventarioCloth',
+  spreadsheetApiKey: 'AIzaSyBTjf-3Xn0A5Uqx035xSmQ63MOrbTCwdco',
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    spreadsheetId: '{SECRET_SPREADSHEETAPIKEY}',
+    spreadsheetName: '{SECRET_SPREADSHEETID}',
+    spreadsheetApiKey: '{SECRET_SPREADSHEETNAME}',
+  }
+  
